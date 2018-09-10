170f5a8fd692bc19b0e10431c8c77cdc76519dc9
OscarOteiza <oscaroteiza@intelligens.ai>
2018-09-10 14:58:40 -0300

    set TOKEN_HOST and CLIENT_ID
    hopefully in a correct way

--- setvars.sh
+++ setvars.sh
@@ -1,7 +1,7 @@
 echo "+++ Set variables."
 # ------------------------------------------------------------------------------
-TOKEN_HOST=your_Twilio_Functions_domain
-CLIENT_ID=default_client_id
+TOKEN_HOST=cinnamon-hamster-1997.twil.io
+CLIENT_ID=AC8802ce81301592cba8b9e12dcf98c737
 export TOKEN_HOST
 export CLIENT_ID
 echo "+ Variables set."
